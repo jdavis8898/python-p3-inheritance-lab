6d326288db29ea9e7c05f495e3b11eb42038a3d7
Jeffrey Davis <j.davis081248@gmail.com>
2024-01-12 09:56:41 -0600

    Done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,17 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name, knowledge=None):
+
+        super().__init__(first_name, last_name)
+
+        if(knowledge is None):
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, knowledge):
+
+        self.knowledge = knowledge
+        
